396aa9fe27afa5470d3805b262683a4ee6e93bce
MohamedElnaggar1223 <maelnaggar1223@gmail.com>
2025-02-21 00:56:48 +0200

    Mobile View

--- components/LandingPage/AgentTest.tsx
+++ components/LandingPage/AgentTest.tsx
@@ -2,20 +2,20 @@ import Image from "next/image";
 
 export default function AgentTest() {
     return (
-        <section className='h-max overflow-y-hidden w-full relative grid max-md:grid-rows-2 md:grid-cols-2 items-center max-w-[100vw] overflow-x-hidden'>
+        <section className='h-max overflow-y-hidden w-full relative grid grid-cols-2 items-center max-w-[100vw] overflow-x-hidden max-md:px-4'>
             <div className='blur-lg z-[-1] radial w-[720px] h-[720px] rounded-full overflow-hidden absolute -left-[11%] -top-[5%]' />
             <div className='blur-lg z-[-1] radial w-[1240px] h-[1240px] rounded-full overflow-hidden absolute -right-[25%] -top-[5%]' />
-            <div className='rounded-3xl bg-[#53545E]/25 flex mx-6 flex-col px-8 py-12 gap-20 max-h-[480px] h-screen w-screen col-span-1'>
+            <div className='rounded-3xl bg-[#53545E]/25 flex md:mx-6 flex-col px-4 py-4 md:px-8 md:py-12 gap-4 md:gap-20 max-h-[140px] md:max-h-[480px] h-screen w-screen col-span-1'>
                 <div className='flex flex-col gap-4'>
-                    <p className='text-white text-5xl xl:text-7xl font-bold'>Free Agent Test!</p>
-                    <p className='text-white text-2xl xl:text-[40px] xl:leading-[48px] font-normal'>Discover your Personality</p>
+                    <p className='text-white text-base xl:text-7xl font-bold'>Free Agent Test!</p>
+                    <p className='text-white text-sm xl:text-[40px] xl:leading-[48px] font-normal'>Discover your Personality</p>
                 </div>
-                <button className='bg-[#F13C3C] drop-shadow-[0px_4px_4px_rgba(251,63,63,0.5)] text-white text-[24px] font-bold px-6 py-2 w-fit rounded-full'>
+                <button className='bg-[#F13C3C] drop-shadow-[0px_4px_4px_rgba(251,63,63,0.5)] text-white text-xs md:text-[24px] font-bold px-4 md:px-6 py-2 md:py-2 w-fit rounded-full'>
                     Let's Get Started!
                 </button>
             </div>
-            <div className='grid grid-cols-4 gap-y-2 max-md:-mt-32'>
-                <div className="relative col-span-1 min-h-[211px]">
+            <div className='grid grid-cols-4 gap-y-2'>
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/omen.png'
                         alt='Omen'
@@ -23,7 +23,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/phonix.png'
                         alt='Phonix'
@@ -31,7 +31,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/jett.png'
                         alt='Jett'
@@ -39,7 +39,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/chamber.png'
                         alt='Chamber'
@@ -47,7 +47,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/raze.png'
                         alt='Raze'
@@ -55,7 +55,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/viper.png'
                         alt='Viper'
@@ -63,7 +63,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/brimstone.png'
                         alt='Brimstone'
@@ -71,7 +71,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/breach.png'
                         alt='Breach'
@@ -79,7 +79,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/astra.png'
                         alt='Astra'
@@ -87,7 +87,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/cypher.png'
                         alt='Cypher'
@@ -95,7 +95,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/kayo.png'
                         alt='Kayo'
@@ -103,7 +103,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/clove.png'
                         alt='Clove'
@@ -111,7 +111,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/tejo.png'
                         alt='Tejo'
@@ -119,7 +119,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/neon.png'
                         alt='Neon'
@@ -127,7 +127,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/reyna.png'
                         alt='Reyna'
@@ -135,7 +135,7 @@ export default function AgentTest() {
                         className='object-contain'
                     />
                 </div>
-                <div className="relative col-span-1 min-h-[211px]">
+                <div className="relative col-span-1 min-h-[60px] md:min-h-[211px]">
                     <Image
                         src='/images/yoru.png'
                         alt='Yoru'
