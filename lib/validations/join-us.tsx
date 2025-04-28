507cea5aae8d82f896bc99b4024c39c7341cfd14
SarahAbdulaziz77 <sarahabdulaziiz76@gmail.com>
2025-04-28 17:57:17 +0300

    phone number error fixes

--- lib/validations/join-us.tsx
+++ lib/validations/join-us.tsx
@@ -10,9 +10,19 @@ export const joinUsSchema = z.object({
     countryCode: z.string().min(1, {
         message: "رمز الدولة مطلوب",
     }),
-    phoneNumber: z.string().min(1, {
-        message: "رقم الهاتف مطلوب",
+
+    phoneNumber: z
+    .string()
+    .regex(/^[0-9]+$/, { message: "رقم الهاتف يجب أن يحتوي على أرقام فقط" })
+    .refine((val) => {
+      if (val.startsWith("0")) {
+        return val.length === 10;
+      }
+      return val.length === 9;
+    }, {
+      message: "رقم الهاتف يجب أن يكون 10 أرقام إذا بدأ ب 0، أو 9 أرقام بدون الصفر",
     }),
+
     role: z.enum(["player", "coach"], {
         required_error: "يرجى تحديد دور",
     }),
