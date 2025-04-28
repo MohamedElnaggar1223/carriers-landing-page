40c9f2bed57b4ab2450225d53779629cbd938a41
SarahAbdulaziz77 <sarahabdulaziiz76@gmail.com>
2025-04-28 18:54:04 +0300

    phone number edit

--- lib/validations/join-us.tsx
+++ lib/validations/join-us.tsx
@@ -13,16 +13,10 @@ export const joinUsSchema = z.object({
 
     phoneNumber: z
     .string()
-    .regex(/^[0-9]+$/, { message: "رقم الهاتف يجب أن يحتوي على أرقام فقط" })
-    .refine((val) => {
-      if (val.startsWith("0")) {
-        return val.length === 10;
-      }
-      return val.length === 9;
-    }, {
-      message: "رقم الهاتف يجب أن يكون 10 أرقام إذا بدأ ب 0، أو 9 أرقام بدون الصفر",
+    .regex(/^[0-9]+$/, {
+      message: "رقم الهاتف يجب أن يحتوي على أرقام فقط",
     }),
-
+    
     role: z.enum(["player", "coach"], {
         required_error: "يرجى تحديد دور",
     }),
