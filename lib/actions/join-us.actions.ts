53d314c5e0a266ba9b62307f47871af7d5aee940
SarahAbdulaziz77 <sarahabdulaziiz76@gmail.com>
2025-04-29 17:42:30 +0300

    link with slak

--- lib/actions/join-us.actions.ts
+++ lib/actions/join-us.actions.ts
@@ -2,6 +2,7 @@
 import { connectDB } from "../mongoose"
 import JoinUs from "@/models/JoinUs"
 import User from "@/models/User"
+import axios from 'axios';
 import { joinUsSchema } from "../validations/join-us"
 import { z } from "zod"
 
@@ -29,6 +30,54 @@ export const createJoinUsEntry = async (data: { fullName: string, email: string,
 
         const newJoinUs = new JoinUs(data)
         await newJoinUs.save()
+
+        await axios.post(process.env.SLACK_WEBHOOK_URL!, {
+            text: ` New Join Us Submission📝`,
+            blocks: [
+              {
+                type: "section",
+                text: {
+                  type: "mrkdwn",
+                  text: `*New submission received!*`,
+                },
+              },
+              {
+                type: "section",
+                fields: [
+                  {
+                    type: "mrkdwn",
+                    text: `*Full Name:*\n${data.fullName}`,
+                  },
+                  {
+                    type: "mrkdwn",
+                    text: `*Email:*\n${data.email}`,
+                  },
+                  {
+                    type: "mrkdwn",
+                    text: `*Phone:*\n${data.countryCode} ${data.phoneNumber}`,
+                  },
+                  {
+                    type: "mrkdwn",
+                    text: `*Game:*\n${data.gameName}`,
+                  },
+                  {
+                    type: "mrkdwn",
+                    text: `*Role:*\n${data.role === 'player' ? 'Player' : 'Coach'}`,
+                  },
+                  {
+                    type: "mrkdwn",
+                    text: `*In Team:*\n${data.isPartOfTeam ? 'Yes' : 'No'}`,
+                  },
+                  ...(data.isPartOfTeam && data.teamName
+                    ? [{
+                        type: "mrkdwn",
+                        text: `*Team Name:*\n${data.teamName}`,
+                      }]
+                    : []),
+                ],
+              },
+            ],
+          });
 
         return { success: true }
     } catch (error) {
